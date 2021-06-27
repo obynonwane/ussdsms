1b533694213b60c7fd47d39abe1310b091fcedc0
obinna <biostechnologyng@gmail.com>
2021-06-27 16:52:13 +0100

    Initial Push request

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+
+    include_once 'menu.php';
+
+    // Read the variables sent via POST from our API
+    $sessionId   = $_POST["sessionId"];
+    $serviceCode = $_POST["serviceCode"];
+    $phoneNumber = $_POST["phoneNumber"];
+    $text        = $_POST["text"];
+
+    $menu = new Menu();
+    $isRegistered = true;
+    $text = $menu->middleware($text);
+    
+
+    if($text == '' && $isRegistered) {
+        //user is registered and string is empty
+        $menu->mainMenuRegistered();
+    }else if($text == '' && !$isRegistered) {
+        //user is unregistered and string is empty
+        $menu->mainMenuUnRegistered();
+    } else if(!$isRegistered) { 
+        //user is unregistered and string is not empty
+        $textArray = explode("*", $text);
+        switch($textArray[0]){
+            case 1:
+                $menu->registerMenu($textArray);
+                break;
+                default:
+                    echo "END Invalid choice. Please try again";
+        }
+    } else {
+        //user is registered and string is not empty
+        $textArray = explode("*", $text);
+        switch($textArray[0]) {
+            case 1:
+                $menu->sendMoneyMenu($textArray);
+                break;
+            case 2:
+                $menu->withdrawMoneyMenu($textArray);
+                break;
+            case 3:
+                $menu->checkBalanceMenu($textArray);
+                break;
+             default:
+                    echo "END Invalid choice. Please try again";
+        }
+    }
---
+++ util.php
@@ -0,0 +1,13 @@
+<?php 
+    class Util {
+        //DB variables 
+
+        //About USSD
+        static $GO_BACK = "98";
+        static $GO_TO_MAIN_MENU = "99";
+        static $SERVER_NAME='localhost';
+        static $DB_NAME='ussdsms';
+        static $DB_USER='root';
+        static $DB_USER_PASS=1234;
+    }
+?>
